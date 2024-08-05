2a08ee0def0419c358231ee982a2e7c957ccfb9c
송민우 <smw0807@gmail.com>
2024-08-06 08:38:04 +0900

    로거 적용

---
+++ src/logger/winston.logger.ts
@@ -0,0 +1,18 @@
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+
+export const winstonLogger = WinstonModule.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        // nestWinstonModuleUtilities.format.nestLike(process.env.PROJECT_NAME),
+        nestWinstonModuleUtilities.format.nestLike('Discrod-Bot'),
+      ),
+    }),
+  ],
+});
